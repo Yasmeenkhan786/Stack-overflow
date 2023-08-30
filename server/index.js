e6a49cf3e2b8c76f138f7b1eb3c2f552fff72c5e
Yasmeenkhan786 <yasmeenkhan99207@gmail.com>
2023-08-30 17:52:49 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,29 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from 'dotenv'
-
+import dotenv from 'dotenv';
 import questionRoutes from './routes/Questions.js'
 import userRoutes from "./routes/users.js";
 import answerRoutes from './routes/Answers.js'
 
+
 const app = express();
 dotenv.config();
+app.use(cors( {
+  origin : "https://stackoverflow-clone-yasmeen.netlify.app",
+  credentials :true
+}));
+app.use(function (req, res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type , Accept");
+  next();
+})
+
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
 
 app.get('/', (req, res) => {
-  res.json("hello")
   res.send("This is a stack overflow clone API yess");
 });
 
