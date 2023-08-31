19462d2ee47230fbe9a50c73fa4701b14ff30316
Yasmeenkhan786 <yasmeenkhan99207@gmail.com>
2023-08-31 16:41:01 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,50 +1,46 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from 'dotenv';
-import questionRoutes from './routes/Questions.js'
+import dotenv from "dotenv";
+import questionRoutes from "./routes/Questions.js";
 import userRoutes from "./routes/users.js";
-import answerRoutes from './routes/Answers.js'
-
+import answerRoutes from "./routes/Answers.js";
 
 const app = express();
 dotenv.config();
-app.use(cors( {
-  origin : "https://stackoverflow-clone-yasmeen.netlify.app",
-  credentials :true
-}));
-app.use(function (req, res,next){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type , Accept");
-  next();
-})
+// app.use(cors( {
+// origin : "https://stackoverflow-clone-yasmeen.netlify.app",
+// credentials :true
+// }));
+// app.use(function (req, res,next){
+// res.header("Access-Control-Allow-Origin", "*");
+// res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type , Accept");
+// next();
+// })
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-
-
-app.get('/', (req, res) => {
+app.use(cors());
+app.get("/", (req, res) => {
   res.send("This is a stack overflow clone API yess");
 });
 
-app.use("/user", userRoutes);
-app.use("/questions", questionRoutes);
-app.use('/answer',answerRoutes)
-
 const PORT = process.env.PORT;
 
-const DATABASE_URL =
- process.env.CONNECTION_URL
+const DATABASE_URL = process.env.CONNECTION_URL;
 mongoose
   .connect(DATABASE_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  
+
   .then(() =>
     app.listen(PORT, () => {
       console.log(`server running on port ${PORT}`);
-      console.log('Done')
+      console.log("Done");
     })
   )
   .catch((err) => console.log(err));
+app.use("/user", userRoutes);
+app.use("/questions", questionRoutes);
+app.use("/answer", answerRoutes);
